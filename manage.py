d3ed80ef636f7ef5ce62f76bb45c0117843dcfd2
Antony <amwaniki180@gmail.com>
2019-07-07 12:55:25 +0300

    migration command

--- manage.py
+++ manage.py
@@ -10,3 +10,20 @@ manager = Manager(app)
 @manager.shell
 def make_shell_context():
     return dict(db = db, app = app, User = User, Post = Post, Comment = Comment)
+
+@manager.command
+def test():
+    """
+    Run the unit tests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+migrate = Migrate(app,db)
+manager.add_command("db", MigrateCommand)
+
+manager.add_command("serve", Server)
+
+if __name__ == "__main__":
+    manager.run()
