8036305e5cb3131e55e11389ae9bafb392a890fc
Antony <amwaniki180@gmail.com>
2019-07-08 09:27:44 +0300

    test instance

--- tests/test_user.py
+++ tests/test_user.py
@@ -18,3 +18,16 @@ class TestUSer(unittest.TestCase):
         """
         User.query.delete()
         Post.query.delete()
+
+     def test_is_instance(self):
+        """
+        This will test whether the user created is an instance of the User class
+        """
+        self.assertTrue(isinstance(self.new_user, User))
+
+    def test_init(self):
+        """
+        This will test whether the user is instantiated correctly
+        """
+        self.assertTrue(self.new_user.username == "antony")
+        self.assertTrue(self.new_user.bio == "haha")
