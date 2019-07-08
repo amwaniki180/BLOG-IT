0d704040600b38610c46d05f6ba336a0ba673b85
Antony <amwaniki180@gmail.com>
2019-07-08 08:48:44 +0300

    importation in main init.py

--- app/_init_.py
+++ app/_init_.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
-# from flask_mail import Mail
-# from flask_uploads import UploadSet,IMAGES,configure_uploads
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_uploads import UploadSet,IMAGES,configure_uploads
 
-# login_manager = LoginManager()
-# login_manager.session_protection = "strong"
-# login_manager.login_view = "auth.login"
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "auth.login"
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 bootstrap = Bootstrap()
-# photos = UploadSet("photos",IMAGES)
-# mail = Mail()
+photos = UploadSet("photos",IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
