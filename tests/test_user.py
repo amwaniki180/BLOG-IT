3f976021cadd33e47079373163198b3e0356aa66
Antony <amwaniki180@gmail.com>
2019-07-08 09:24:57 +0300

    teardown

---
+++ tests/test_user.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import User,Post
+from app import db
+class TestUSer(unittest.TestCase):
+    """
+    This is the class we will be using to test the users
+    """
+
+    def setUp(self):
+        """
+        This will create a new user before each test
+        """
+        self.new_user = User(username = "Marvin",bio = "haha",password ="marvin")
+
+    def tearDown(self):
+        """
+        This will clear the db after each test
+        """
+        User.query.delete()
+        Post.query.delete()
